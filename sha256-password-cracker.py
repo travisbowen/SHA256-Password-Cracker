842834b694e8070c3f58c8df4a77c3be7699c731
Travis Bowen <Travisbowen23@gmail.com>
2022-08-19 14:45:38 -0600

    Polished off script

--- sha256-password-cracker.py
+++ sha256-password-cracker.py
@@ -1,22 +1,19 @@
 from pwn import *
-import sys
 
-hashToCrack = input("Enter hash needed to be cracked:\n ")
-password_file = input(
-    "Enter filename of passwords to compare to hash: ex.passwordList.txt\n ").strip("\n")
+hashToCrack = input("Enter hash needed to be cracked:\n ").strip("\n").replace(" ", "")
+password_file = input("Enter filename of passwords to compare to hash: ex.passwordList.txt\n ")
+
 
 # Prints the hash that is being cracked
-with log.progess("Attempting to crack: {}!\n".format(hashToCrack)) as p:
-    # Enter filename of password dictionary file to compare against hash
-    with open(password_file, "r", encoding='latin-1') as password_list:
-        for password in password_list:
-            password = password.strip('\n').encode('latin-1')
-            # Calculates the sha256 sum of a string; returns hex-encoded
-            password_hash = sha256sumhex(password)
-            p.status("Trying password: {} hashed as {}".format(
-                password.decode('latin-1'), password_hash))
-            if password_hash == hashToCrack:
-                p.success(
-                    "[+] Password found: {}".format(password.decode('latin-1')))
-                exit()
-        p.failure("[X] Password hash not found!")
+with log.progress("Attempting to crack: {}".format(hashToCrack)) as progress:
+  # Enter filename of password dictionary file to compare against hash
+  with open(password_file.strip("\n"), "r", encoding='latin-1') as password_list:
+    for password in password_list:
+      password = password.strip('\n').encode('latin-1')
+      # Calculates the sha256 sum of a string - returns hex-encoded
+      password_hash = sha256sumhex(password)
+      progress.status('Hacking ‍💻.....')
+      if password_hash == hashToCrack:
+        progress.success("[+] Password found: {}".format(password.decode('latin-1')))
+        exit()
+    progress.failure("[X] Password hash not found!")
