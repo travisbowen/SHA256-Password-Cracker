fea68c684604a952732e183ff686024729b90ed4
Travis Bowen <Travisbowen23@gmail.com>
2022-08-19 12:36:24 -0600

    Updated input for filename to be passed in

--- sha256-password-cracker.py
+++ sha256-password-cracker.py
@@ -2,11 +2,13 @@ from pwn import *
 import sys
 
 hashToCrack = input("Enter hash needed to be cracked:\n ")
+password_file = input(
+    "Enter filename of passwords to compare to hash: ex.passwordList.txt\n ").strip("\n")
 
 # Prints the hash that is being cracked
 with log.progess("Attempting to crack: {}!\n".format(hashToCrack)) as p:
     # Enter filename of password dictionary file to compare against hash
-    with open(input("Enter filename of passwords to compare to hash: ex.passwordList.txt\n ").strip("\n"), "r", encoding='latin-1') as password_list:
+    with open(password_file, "r", encoding='latin-1') as password_list:
         for password in password_list:
             password = password.strip('\n').encode('latin-1')
             # Calculates the sha256 sum of a string; returns hex-encoded
